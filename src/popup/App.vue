e162d6667966160bb0909da407e68a15bd1fc362
Milen Radkov <milenradkov@me.com>
2019-05-14 09:30:41 +0300

    balance polling

--- src/popup/App.vue
+++ src/popup/App.vue
@@ -12,7 +12,6 @@
         <!-- network -->
         <ae-dropdown slot="mobile-right" direction="right" class="mr-2">
           <ae-button slot="button" class="top-button">
-            <p class="p-top" v-if="!currentNetwork">Network</p>
             <p class="p-top status" v-if="currentNetwork == 'testnet'">
               Testnet
             </p>
