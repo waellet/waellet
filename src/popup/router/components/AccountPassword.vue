2692d5e2ed23853d4c940c4ceb3f6e748fff5189
Petar <petbaik26@gmail.com>
2019-06-03 18:08:17 +0300

    Added views for Import secret key page

---
+++ src/popup/router/components/AccountPassword.vue
@@ -0,0 +1,32 @@
+<template>
+    <div >
+        <ae-modal
+            v-if="accountPasswordVisible"
+            @close="accountPasswordVisible = false"
+            title="Account Password">
+            <ae-input  v-model="accountPassword" placeholder="" class="my-2" label="Password" />
+            <ae-input  v-model="accountPassword" placeholder="" class="my-2" label="Repeat Password" />
+            <ae-button face="round" extend fill="seconday" >Import</ae-button>
+        </ae-modal> 
+    </div>
+</template>
+
+<script>
+import locales from '../../locales/locales.json';
+export default {
+    props: ['accountPasswordVisible','secretKey'],
+    data() {
+        return {
+           
+        }
+    },
+    locales,
+    methods: {
+    }
+}
+
+</script>
+<style lang="scss" scoped>
+@import '../../../common/base';
+
+</style>
--- src/popup/router/pages/Index.vue
+++ src/popup/router/pages/Index.vue
@@ -26,13 +26,53 @@
       </div>
     </footer>
 
+    <accountPassword 
+      @clickAction="importPrivateKey" 
+      :accountPasswordVisible="accountPasswordVisible"
+      :secretKey="secretKey" />
+
     <ae-modal
       v-if="modalVisible"
       @close="modalVisible = false"
-      title="Import secret key"
-    >
-      <ae-input v-model="secretKey" placeholder="Input secret key" />
-      <ae-button face="round" extend fill="seconday" @click="importPrivateKey(secretKey)">Import</ae-button>
+      title="Import secret key">
+
+      <div class="tabs">
+        <span @click="switchImportType('privateKey')" :class="{'tab-active':importType == 'privateKey'}">Private key</span>
+        <span @click="switchImportType('keystore')"  :class="{'tab-active':importType == 'keystore'}">Keystore.json</span>
+        <span @click="switchImportType('seedPhrase')"  :class="{'tab-active':importType == 'seedPhrase'}">Seed phrase</span>
+      </div>
+
+      <ae-input 
+        label="Secret Key"
+        v-if="importType == 'privateKey'" 
+        v-model="secretKey" 
+        v-bind="inputError"
+        class="my-2" >
+        <ae-toolbar slot="footer">
+          This field is required
+        </ae-toolbar>
+      </ae-input>
+
+      <input  type="file" v-if="importType == 'keystore'" placeholder="Upload keystore.json" class="my-2 input" />
+      
+      <div v-if="importType == 'seedPhrase'">
+        <p>Enter your seed phrase. The one you wrote down during account creation. </p>
+        <!--<ae-textarea  v-model="seedPhrase" placeholder="Enter seed phrase" class="my-2  input" />-->
+          <ae-input label="Seed phrase" class="my-2" >
+            <textarea class="textarea"
+              slot-scope="{ context }"
+              @focus="context.focus = true" 
+              @blur="context.focus = false" 
+            />
+            <ae-toolbar slot="footer">
+              This field is required
+            </ae-toolbar>
+          </ae-input>
+      </div>
+
+      <ae-button face="round" extend fill="secondary" @click="importShowPassword(secretKey)">Continue</ae-button>
+
+      <!--<ae-button face="round" disabled extend fill="seconday" @click="importPrivateKey(secretKey)">Import</ae-button>-->
     </ae-modal>
   </div>
 </template>
@@ -49,7 +89,11 @@ export default {
       loading: false,
       heading: '',
       modalVisible: false,
-      logo: chrome.runtime.getURL('../../../icons/icon_128.png')
+      logo: chrome.runtime.getURL('../../../icons/icon_128.png'),
+      importType:'privateKey',
+      showPassword: false,
+      accountPasswordVisible:false,
+      inputError:''
     };
   },
   locales,
@@ -88,22 +132,34 @@ export default {
         this.$router.push('/account');
       });
     },
-    importPrivateKey: async function importPrivateKey(secretKey) {
+    importPrivateKey: async function importPrivateKey({accountPassword,secretKey}) {
       this.modalVisible = false;
       this.loading = true;
-      const keyPair = await addressGenerator.importPrivateKey('test', secretKey);
+      this.accountPasswordVisible = false;
+      const keyPair = await addressGenerator.importPrivateKey(accountPassword, secretKey);
       chrome.storage.sync.set({userAccount: keyPair}, () => {
         this.$store.commit('UPDATE_ACCOUNT', keyPair);
         this.$router.push('/account');
       });
     },
+    switchImportType(type) {
+      this.importType = type;
+    },
+    importShowPassword(secretKey) {
+      if(secretKey.length == 128) {
+        this.accountPasswordVisible = true;
+        this.modalVisible = false;
+        this.inputError = '';
+      }else {
+        this.inputError = 'error';
+      }
+    }
   },
 };
 </script>
 
 <style lang="scss" scoped>
 @import '../../../common/base';
-
 main {
   padding-top: 1rem;
   padding-bottom: 2rem;
@@ -132,4 +188,27 @@ main {
   margin: 0 auto;
   padding: 0 3rem;
 }
+.tabs, .date {
+    margin-left: rem(-48px);
+    margin-right: rem(-48px);
+    padding-left: rem(48px);
+}
+.tabs {
+  box-shadow: 0 4px 2px -2px rgba(27, 68, 121, 0.1);
+  span {
+    cursor: pointer;
+    display:inline-block;
+    width:32%;
+    font-weight: bold;
+    text-align: center;
+    color: #203040;
+    &.tab-active {
+      padding-bottom: 0;
+      border-bottom: 2px solid #FF0D6A;
+    }
+    
+  }
+}
+
+
 </style>
