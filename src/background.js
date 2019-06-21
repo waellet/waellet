e7b1547654987cf961d5474ecc732b9c293efba2
Petar Baykov <petbaik26@gmail.com>
2019-06-21 10:55:05 +0300

    Comment extension provider because it throws errors

--- src/background.js
+++ src/background.js
@@ -1,6 +1,6 @@
 import MemoryAccount from '@aeternity/aepp-sdk/es/account/memory'
 import Account from '@aeternity/aepp-sdk/es/account'
-import ExtensionProvider from '@aeternity/aepp-sdk/es/provider/extension'
+// import ExtensionProvider from '@aeternity/aepp-sdk/es/provider/extension'
 
 
 global.browser = require('webextension-polyfill');
@@ -30,100 +30,100 @@ function getAccount() {
 }
 
 
-getAccount()
-    .then((account) => {
-        // Init accounts
-        const accounts = [
-            // You can add your own account implementation,
-            // Account.compose({
-            //     init() {
-            //     },
-            //     methods: {
-            //         /**
-            //          * Sign data blob
-            //          * @function sign
-            //          * @instance
-            //          * @abstract
-            //          * @category async
-            //          * @rtype (data: String) => data: Promise[String]
-            //          * @param {String} data - Data blob to sign
-            //          * @return {String} Signed data blob
-            //          */
-            //         async sign(data) {
-            //         },
-            //         /**
-            //          * Obtain account address
-            //          * @function address
-            //          * @instance
-            //          * @abstract
-            //          * @category async
-            //          * @rtype () => address: Promise[String]
-            //          * @return {String} Public account address
-            //          */
-            //         async address() {
-            //         }
-            //     }
-            // })(),
-            MemoryAccount(account)
-        ]
-        return accounts
-    })
-    .then((accounts) => {
-        // Init extension stamp from sdk
-        ExtensionProvider({
-            // Provide post function (default: window.postMessage)
-            postFunction: postToContent,
-            // By default `ExtesionProvider` use first account as default account. You can change active account using `selectAccount (address)` function
-            accounts: accounts,
-            // Hook for sdk registration
-            onSdkRegister: function (sdk) {
-                // sendDataToPopup(this.getSdks())
-                // if (confirm('Do you want to share wallet with sdk ' + sdk.sdkId)) sdk.shareWallet()
-                sdk.shareWallet();
-                chrome.storage.sync.set({showAeppPopup:{ data: sdk.sdkId.toString(), type:'confirm'  } } , () => {
-                    chrome.windows.create({
-                        url: chrome.runtime.getURL('./popup/popup.html'),
-                        type: "popup",
-                        height: 600,
-                        width:420
-                      },() => {
-                        console.log("created");
+// getAccount()
+//     .then((account) => {
+//         // Init accounts
+//         const accounts = [
+//             // You can add your own account implementation,
+//             // Account.compose({
+//             //     init() {
+//             //     },
+//             //     methods: {
+//             //         /**
+//             //          * Sign data blob
+//             //          * @function sign
+//             //          * @instance
+//             //          * @abstract
+//             //          * @category async
+//             //          * @rtype (data: String) => data: Promise[String]
+//             //          * @param {String} data - Data blob to sign
+//             //          * @return {String} Signed data blob
+//             //          */
+//             //         async sign(data) {
+//             //         },
+//             //         /**
+//             //          * Obtain account address
+//             //          * @function address
+//             //          * @instance
+//             //          * @abstract
+//             //          * @category async
+//             //          * @rtype () => address: Promise[String]
+//             //          * @return {String} Public account address
+//             //          */
+//             //         async address() {
+//             //         }
+//             //     }
+//             // })(),
+//             MemoryAccount(account)
+//         ]
+//         return accounts
+//     })
+//     .then((accounts) => {
+//         // Init extension stamp from sdk
+//         ExtensionProvider({
+//             // Provide post function (default: window.postMessage)
+//             postFunction: postToContent,
+//             // By default `ExtesionProvider` use first account as default account. You can change active account using `selectAccount (address)` function
+//             accounts: accounts,
+//             // Hook for sdk registration
+//             onSdkRegister: function (sdk) {
+//                 // sendDataToPopup(this.getSdks())
+//                 // if (confirm('Do you want to share wallet with sdk ' + sdk.sdkId)) sdk.shareWallet()
+//                 sdk.shareWallet();
+//                 chrome.storage.sync.set({showAeppPopup:{ data: sdk.sdkId.toString(), type:'confirm'  } } , () => {
+//                     chrome.windows.create({
+//                         url: chrome.runtime.getURL('./popup/popup.html'),
+//                         type: "popup",
+//                         height: 600,
+//                         width:420
+//                       },() => {
+//                         console.log("created");
                         
-                    });
-                });
+//                     });
+//                 });
 
                 
-            },
-            // Hook for signing transaction
-            onSign: function ({sdkId, tx, txObject, sign}) {
-                // sendDataToPopup(this.getSdks())
-                // if (confirm('Do you want to sign ' + JSON.stringify(txObject) + ' ?')) sign() // SIGN TX
-                sign();
-                chrome.storage.sync.set({showAeppPopup:{ data: txObject, type:'sign'  } } , () => {
-                    chrome.windows.create({
-                        url: chrome.runtime.getURL('./popup/popup.html'),
-                        type: "popup",
-                        height: 600,
-                        width:420
-                      },() => {
-                        console.log("created");
-                    });
-                });
-            }
-        }).then(provider => {
-            // Subscribe from postMessages from page
-            chrome.runtime.onMessage.addListener((msg, sender) => {
-                switch (msg.method) {
-                    case 'pageMessage':
-                        console.log(msg);
-                        provider.processMessage(msg);
-                        break
-                }
-            })
-        }).catch(err => {
-            console.error(err)
-        })
-    });
+//             },
+//             // Hook for signing transaction
+//             onSign: function ({sdkId, tx, txObject, sign}) {
+//                 // sendDataToPopup(this.getSdks())
+//                 // if (confirm('Do you want to sign ' + JSON.stringify(txObject) + ' ?')) sign() // SIGN TX
+//                 sign();
+//                 chrome.storage.sync.set({showAeppPopup:{ data: txObject, type:'sign'  } } , () => {
+//                     chrome.windows.create({
+//                         url: chrome.runtime.getURL('./popup/popup.html'),
+//                         type: "popup",
+//                         height: 600,
+//                         width:420
+//                       },() => {
+//                         console.log("created");
+//                     });
+//                 });
+//             }
+//         }).then(provider => {
+//             // Subscribe from postMessages from page
+//             chrome.runtime.onMessage.addListener((msg, sender) => {
+//                 switch (msg.method) {
+//                     case 'pageMessage':
+//                         console.log(msg);
+//                         provider.processMessage(msg);
+//                         break
+//                 }
+//             })
+//         }).catch(err => {
+//             console.error(err)
+//         })
+//     });
 
 const postToContent = (data) => {
     chrome.tabs.query({}, function (tabs) { // TODO think about direct communication with tab
