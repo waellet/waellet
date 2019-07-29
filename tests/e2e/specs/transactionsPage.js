59d5bfc95d7fbe8ae0b4f9a4da3c0f74a0ac259c
Petar Baykov <petbaik26@gmail.com>
2019-07-29 15:45:29 +0300

    Update transactionsPage.js

--- tests/e2e/specs/transactionsPage.js
+++ tests/e2e/specs/transactionsPage.js
@@ -34,7 +34,7 @@ describe("Tets cases for Transactions Page", () => {
                     cy.wrap(elem).should('have.class',data.hash);
                     cy.wrap(elem).find('.ae-address').should('have.attr','title',address);
                     cy.wrap(elem).find('.transactionDate').should('contain',new Date(data.time).toLocaleTimeString() );
-                    if (data.tx.type != "NameClaimTx" && data.tx.type != 'NamePreclaimTx' && data.tx.type != 'NameUpdateTx'){
+                    if (data.tx.type != "NameClaimTx" && data.tx.type != 'NamePreclaimTx' && data.tx.type != 'NameUpdateTx' && data.tx.type != 'ContractCreateTx'){
                         cy.wrap(elem).find('div.balance').should('contain',amount);
                         cy.wrap(elem).find('small .balance').should('contain',fee);
                     }
@@ -58,7 +58,7 @@ describe("Tets cases for Transactions Page", () => {
             let fee = data.tx.fee / 10 ** 18;
             let total = (parseFloat(amount) + parseFloat(fee)).toFixed(7);
             cy.get('.allTransactions .list-item-transaction').eq(0).click().then(elem => {
-                if (data.tx.type != "NameClaimTx" && data.tx.type != 'NamePreclaimTx' && data.tx.type != 'NameUpdateTx'){
+                if (data.tx.type != "NameClaimTx" && data.tx.type != 'NamePreclaimTx' && data.tx.type != 'NameUpdateTx' && data.tx.type != 'ContractCreateTx'){
                     cy.get('body').find('.transactionDate').should('contain',new Date(data.time).toLocaleTimeString() );
                     // cy.get('body').find('.transactionType').should('contain',data.tx.type)
                     cy.get('body').find('.transactionAmount').should('contain',amount);
